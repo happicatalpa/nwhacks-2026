d4acab9018114aca70fbba46e7345a716396256e
Gina Liu <ginatyliu@gmail.com>
2026-01-17 12:50:44 -0800

    camera test

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Camera from './components/Camera.jsx';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,17 +11,9 @@ function App() {
   return (
     <>
       <h1>Presentation demo!!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div> 
+        <Camera />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/Camera.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useRef } from "react";
+
+export default function Camera() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(stream => {
+        videoRef.current.srcObject = stream;
+      });
+  }, []);
+
+  return <video ref={videoRef} autoPlay playsInline />;
+}
