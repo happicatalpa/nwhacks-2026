c50247fb554a47a3a7c08e8519859b761e9399cd
geogr4ze <upwzt2007@gmail.com>
2026-01-17 17:16:31 -0800

    button transition

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,11 +1,13 @@
 
 
 export default function Home() {
+  
+
   return (
     <div id="home"> 
         <h1>Happy Yapper</h1>
-        <button id="home">START</button>
-        <button id="home">Help</button>
+        <button id="large">START</button>
+        <button id="large">Help</button>
     </div>
   );
 }
