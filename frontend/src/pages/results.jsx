51b17d24d86cd6be2f43fe9526de52613f1556bc
aisling lu <aislinglu2@gmail.com>
2026-01-17 21:18:04 -0800

    updated imgs

--- frontend/src/pages/results.jsx
+++ frontend/src/pages/results.jsx
@@ -2,8 +2,11 @@ export default function Results() {
   return (
     <div> 
         <h1>WOOHOO you finished your cool talk</h1>
-        <p>SCORE: </p>
+        <div className = "">
+            <p>SCORE: </p>
+        </div>
         <button id="small">Try Again</button>
+        <img src = "/yaptrainer.png"></img>
     </div>
   );
 }
