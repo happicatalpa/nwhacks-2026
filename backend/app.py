07db482620a58fe610a7202efcc2ce9a074d4a7b
Gina Liu <ginatyliu@gmail.com>
2026-01-17 17:03:10 -0800

    adding gemini

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,27 @@
 from flask import Flask, jsonify, request
+from gemini_client import request_key_points
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return jsonify({"message": "Hello from Flask!"})
+
+@app.route("/get-key-points", methods=["POST"])
+def get_key_points():
+    data = request.get_json
+    script = data.get('script')
+    if not script:
+        return jsonify({"error": "Missing script"}), 400
+
+    result = request_key_points(script, transcript)
+
+    # If error happened inside send_to_gemini
+    if "error" in result:
+        return jsonify(result), 500
+
+    return jsonify(result)
+
 
 @app.route("/compare-scripts", methods=["POST"])
 def compare_scripts():
