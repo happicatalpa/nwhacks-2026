8e4db627fdc2160e5467341c5eb8b45ce51f62c8
aisling lu <aislinglu2@gmail.com>
2026-01-17 17:47:08 -0800

    merge conflict error fixed

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,12 +1,8 @@
 
 
 export default function Home() {
-<<<<<<< HEAD
   
 
-=======
-    
->>>>>>> 254527271c14474f9ae8945ab0bb0129bf9647ce
   return (
     <div id="home"> 
         <h1>Happy Yapper</h1>
