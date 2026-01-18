ad3889b917775b158a39dbb6042667648b654110
Emmy <emthai8@gmail.com>
2026-01-17 21:44:31 -0800

    the back to start button works

--- frontend/src/pages/results.jsx
+++ frontend/src/pages/results.jsx
@@ -1,12 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 export default function Results() {
+
   return (
     <div> 
         <h1>WOOHOO you finished your cool talk</h1>
         <div className = "">
             <p>SCORE: </p>
         </div>
-        <button id="small">Try Again</button>
+
+        <Link to="/">
+          <button id="small">Back to Start</button>
+        </Link>
+        
         <img src = "/yaptrainer.png"></img>
     </div>
+
   );
 }
