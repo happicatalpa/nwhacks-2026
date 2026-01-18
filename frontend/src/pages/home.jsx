7a3fa42fb04344567634c7af237eb59ea9c5c252
aisling lu <aislinglu2@gmail.com>
2026-01-17 22:47:22 -0800

    changed homepage

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
 
-  return (
-    <div id="home"> 
-        <h1>Happy Yapper</h1>
-        <Link to="/setup">
-            <button id="large">START</button>
-        </Link>
-        <button id="large">Help</button>
-    </div>
-  );
+    return (
+        <div id="home">
+            <h1 id="name">Happy <br /> Yapper</h1>
+            <div id="home-buttons">
+                <Link to="/setup">
+                    <button id="large">START</button>
+                </Link>
+                <button id="large">Help</button>
+            </div>
+            <img src="/background.png" id="background"></img>
+        </div>
+    );
 }
